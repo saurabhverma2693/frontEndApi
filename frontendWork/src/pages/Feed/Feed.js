2aeb4095d582da298b2b354efef6398075e9e7ca
saurabh <saurabhverma2693@gmail.com>
2025-06-19 18:22:04 +0530

    Async and await in backend

--- frontendWork/src/pages/Feed/Feed.js
+++ frontendWork/src/pages/Feed/Feed.js
@@ -22,7 +22,11 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -50,10 +54,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page 
-, {headers: {
-      Authorization : 'Bearer ' + this.props.token
-    }})
+    fetch('http://localhost:8080/feed/posts?page=' + page, {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -77,7 +82,16 @@ class Feed extends Component {
 
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        status: this.state.status
+      })
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
@@ -128,8 +142,8 @@ class Feed extends Component {
       method: method,
       body: formData,
       headers: {
-      Authorization : 'Bearer ' + this.props.token
-    }
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -184,8 +198,8 @@ class Feed extends Component {
     fetch('http://localhost:8080/feed/post/' + postId, {
       method: 'DELETE',
       headers: {
-      Authorization : 'Bearer ' + this.props.token
-    }
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
--- frontendWork/src/pages/Feed/SinglePost/SinglePost.js
+++ frontendWork/src/pages/Feed/SinglePost/SinglePost.js
@@ -14,10 +14,10 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('http://localhost:8080/feed/post/' + postId,{
+    fetch('http://localhost:8080/feed/post/' + postId, {
       headers: {
-      Authorization : 'Bearer ' + this.props.token
-    }
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200) {
