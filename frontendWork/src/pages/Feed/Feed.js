526276afc648a454cfdfa56d08322152cd5f9667
saurabh <saurabhverma2693@gmail.com>
2025-06-19 17:23:24 +0530

    Api Done

--- frontendWork/src/pages/Feed/Feed.js
+++ frontendWork/src/pages/Feed/Feed.js
@@ -50,7 +50,10 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page)
+    fetch('http://localhost:8080/feed/posts?page=' + page 
+, {headers: {
+      Authorization : 'Bearer ' + this.props.token
+    }})
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -123,7 +126,10 @@ class Feed extends Component {
 
     fetch(url, {
       method: method,
-      body: formData
+      body: formData,
+      headers: {
+      Authorization : 'Bearer ' + this.props.token
+    }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -176,7 +182,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/' + postId, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+      Authorization : 'Bearer ' + this.props.token
+    }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -255,7 +264,8 @@ class Feed extends Component {
                 <Post
                   key={post._id}
                   id={post._id}
-                  author={post.creator.name}
+                  // author={post.creator.name}
+                  author={post.creator ? post.creator.name : 'Unknown'}
                   date={new Date(post.createdAt).toLocaleDateString('en-US')}
                   title={post.title}
                   image={post.imageUrl}
